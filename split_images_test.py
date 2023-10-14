a7413b1685d96501dbb4f81c469b85aa3a980c1b
Binglin Shen <59431309+shenblin@users.noreply.github.com>
2023-10-14 14:19:20 +0800

    Add files via upload

---
+++ split_images_test.py
@@ -0,0 +1,92 @@
+import os
+import glob
+import cv2
+
+input_folder = 'Ovarian/test/ovarian_RGB'
+# input_folder = 'Ovarian/test/ovarian_G_R'
+# input_folder = 'Ovarian/test/ovarian_R'
+# input_folder = 'Ovarian/test/ovarian_B_R'
+# input_folder = 'Ovarian/test/ovarian_B_G'
+# input_folder = 'Ovarian/test/ovarian_B'
+# input_folder = 'Ovarian/test/subtract_G-R'
+
+GT_folder = 'Ovarian/test/ovarian_GT'
+
+
+tile_pixel = 272
+overlap = 136
+
+
+input_imgs_path = 'datasets/{}'.format(input_folder)
+input_tiles_path = 'datasets/test_dataset/{}'.format(input_folder)
+GT_imgs_path = 'datasets/{}'.format(GT_folder)
+GT_tiles_path = 'datasets/test_dataset/{}'.format(input_folder)
+
+
+def crop_im(imgs_path, tiles_path, testAB, overlap, reverse_color='fault', RGB2gray='fault'):
+    im_files = sorted(list(glob.glob(os.path.join(imgs_path, '*.tif'))))
+
+    for k, im_file0 in enumerate(im_files):
+        # if k == break_num:
+        #     break
+        im_file = os.path.split(im_file0)[-1].split('.')[0]
+        im = cv2.imread(im_file0, -1)
+        if reverse_color == 'true':
+            # reverse color
+            im = 255 - im
+
+        h, w = im.shape[0:2]
+        h_num = h // tile_pixel
+        w_num = w // tile_pixel
+        print('Image tile number: %.2f x %.2f' % (h / tile_pixel, w / tile_pixel))
+
+        for i in range(h_num):
+            for j in range(w_num):
+                num = w_num * i + j
+                if i == 0:
+                    h_start = tile_pixel * i
+                elif i == h_num - 1:
+                    h_start = tile_pixel * i - 2 * overlap
+                else:
+                    h_start = tile_pixel * i - overlap
+                if j == 0:
+                    w_start = tile_pixel * j
+                elif j == w_num - 1:
+                    w_start = tile_pixel * j - 2 * overlap
+                else:
+                    w_start = tile_pixel * j - overlap
+                if i == h_num - 1:
+                    h_end = tile_pixel * (i + 1)
+                elif i == 0:
+                    h_end = tile_pixel * (i + 1) + 2 * overlap
+                else:
+                    h_end = tile_pixel * (i + 1) + overlap
+                if j == w_num - 1:
+                    w_end = tile_pixel * (j + 1)
+                elif j == 0:
+                    w_end = tile_pixel * (j + 1) + 2 * overlap
+                else:
+                    w_end = tile_pixel * (j + 1) + overlap
+                image_tile = im[h_start:h_end, w_start:w_end]
+
+                save_tiles_path = os.path.join(tiles_path, im_file, testAB)
+                trainA_path = os.path.join(tiles_path, im_file, 'trainA')
+                trainB_path = os.path.join(tiles_path, im_file, 'trainB')
+                if not os.path.exists(save_tiles_path):
+                    os.makedirs(save_tiles_path)
+                if not os.path.exists(trainA_path):
+                    os.makedirs(trainA_path)
+                if not os.path.exists(trainB_path):
+                    os.makedirs(trainB_path)
+                new_im_path = os.path.join(save_tiles_path, (str(num) + '.tif'))
+
+                if RGB2gray == 'true':
+                    # convert RGB image into gray
+                    image_tile = cv2.cvtColor(image_tile, cv2.COLOR_RGB2GRAY)
+                cv2.imwrite(new_im_path, image_tile)
+
+
+crop_im(input_imgs_path, input_tiles_path, 'testA', overlap, 'true')
+crop_im(GT_imgs_path, GT_tiles_path, 'testB', overlap)
+
+print('done!')
---
+++ stitch_tiles.py
@@ -0,0 +1,95 @@
+import os
+import glob
+import cv2
+from functools import cmp_to_key
+import numpy as np
+
+epoch = '80'
+# epoch = 'latest'
+
+num_imgs = 8
+
+# name = 'MP_to_HE_cross_contrastive'
+# name = 'MP_to_HE_unet_256'
+# name = 'MP_to_HE_stylegan2'
+# name = 'MP_to_HE_res_6'
+
+name = 'MP_to_HE'
+# name = '2PA_SHG_to_HE'
+# name = '2PA_to_HE'
+# name = '2P_3PA_to_HE'
+# name = '3PA_SHG_to_HE'
+# name = '3PA_to_HE'
+# name = '2H_to_HE'
+
+h_num, w_num = 8, 8
+overlap = 136
+# h_num, w_num = 8, 8 # for unet
+# overlap = 128
+
+def stitch_image_tiles(tiles_path, stitched_imgs_path, sample, rev_color='fault'):
+    if not os.path.exists(stitched_imgs_path):
+        os.makedirs(stitched_imgs_path)
+    im_files = sorted(list(glob.glob(os.path.join(tiles_path, '*.png'))))
+    im_names = [os.path.split(im_file0)[-1].split('.')[0] for im_file0 in im_files]
+    im_names_files = {}
+    im_names_ = []
+    for im_name, im_file in zip(im_names, im_files):
+        im_names_files[im_name] = im_file
+        im_names_.append(im_name)
+    im_names = im_names_
+
+    h_s, w_s = -1, -1
+    for i_, im_name in enumerate(im_names):
+        im_file0 = im_names_files[im_name]
+        im = cv2.imread(im_file0)
+        im_num = int(im_name.split('_')[-1])
+        i = im_num // w_num  # i row idx
+        j = im_num % w_num  # j col idx
+        if overlap != 0:
+            if i == 0:
+                im = im[:-2 * overlap, :]
+            elif i == h_num - 1:
+                im = im[2 * overlap:, :]
+            else:
+                im = im[overlap:-overlap, :]
+            if j == 0:
+                im = im[:, :-2 * overlap]
+            elif j == w_num - 1:
+                im = im[:, 2 * overlap:]
+            else:
+                im = im[:, overlap:-overlap]
+        h, w = im.shape[0:2]
+        if i_ % (h_num * w_num) == 0:
+            h_s, w_s = h, w
+            h_t, w_t = h_num * h_s, w_num * w_s
+            im_t = np.zeros(shape=(h_t, w_t, 3))
+        assert h == h_s, w == w_s
+        im_t[h_s * i:h_s * (i + 1), w_s * j:w_s * (j + 1)] = im
+        if (i_ + 1) % (h_num * w_num) == 0:
+            if rev_color == 'true':
+                # reverse color
+                im_t = 255 - im_t
+            im_name_t_path = os.path.join(stitched_imgs_path, (sample + '.png'))
+            cv2.imwrite(im_name_t_path, im_t)
+
+
+
+for i in range(12, num_imgs):
+    result_imgs_path = 'results/{}/stitched_epoch_{}'.format(name, epoch)
+
+    sample = 'ovarian_{}_MP'.format(str(i + 1))
+    imgs_path = 'result_tiles/{}/{}/test_{}/images/real_A'.format(str(i + 1), name, epoch)
+    stitch_image_tiles(imgs_path, result_imgs_path, sample, 'true')
+
+    sample = 'ovarian_{}_NODH'.format(str(i + 1))
+    imgs_path = 'result_tiles/{}/{}/test_{}/images/fake_B'.format(str(i + 1), name, epoch)
+    stitch_image_tiles(imgs_path, result_imgs_path, sample)
+
+    sample = 'ovarian_{}_HE'.format(str(i + 1))
+    imgs_path = 'result_tiles/{}/{}/test_{}/images/real_B'.format(str(i + 1), name, epoch)
+    stitch_image_tiles(imgs_path, result_imgs_path, sample)
+    print('saving im %d' % (i+1))
+
+
+print('done!')
